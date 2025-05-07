6ef7a46396de9146e5934264ae592a32474e0b83
Juna <jpdizon5@up.edu.ph>
2025-05-07 13:23:54 +0800

    Update start.sh to install enchant dependencies

--- start.sh
+++ start.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
+# Install system dependencies
+apt-get update
+apt-get install -y libenchant-2-2
+
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Start the bot
 python bot.py
