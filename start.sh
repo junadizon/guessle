bc0809b16bf03fcdcb2bc502654e5c8b12eccea7
Juna <jpdizon5@up.edu.ph>
2025-05-07 13:29:04 +0800

    Add sudo to system package installation

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Install system dependencies
-apt-get update
-apt-get install -y libenchant-2-2
+sudo apt-get update
+sudo apt-get install -y libenchant-2-2
 
 # Install Python dependencies
 pip install -r requirements.txt
