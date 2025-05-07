cca6d1509ac59442ad2b4ee41c54890ddd091ec6
Juna <jpdizon5@up.edu.ph>
2025-05-07 13:50:24 +0800

    debugging

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Install system dependencies
-apt update
-apt install -y libenchant-2-2
+apt-get update
+apt-get install -y libenchant-2-2
 
 # Install Python dependencies
 pip install -r requirements.txt
